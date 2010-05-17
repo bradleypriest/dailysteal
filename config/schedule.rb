ff5e7d4292ad86cd89c62445f2b1db51dc32fc0e
Bradley Priest <bradleypriest@gmail.com>
2010-05-18 11:56:30 +1200

    cror

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
-every 1.day, :at => '12:01 pm' do
-  runner 'FeedEntry.update_from("http://feed.1-day.co.nz/1-day")'
+every 1.day, :at => '11:59 pm' do
+  runner 'FeedEntry.update_from_feed("http://feed.1-day.co.nz/1-day")'
 end
 
-every 1.day, :at => '11:06 pm' do
+every 1.day, :at => '11:59 am' do
   runner 'FeedEntry.update_from_feed("http://feeds.feedburner.com/offtheback")'
 end
