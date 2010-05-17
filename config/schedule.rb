c9b716f9ffb2fddaf200ae2b1cff0cf7a900fbad
Bradley Priest <bradleypriest@gmail.com>
2010-05-18 10:53:20 +1200

    when

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,7 @@
 every 1.day, :at => '12:01 pm' do
   runner 'FeedEntry.update_from("http://feed.1-day.co.nz/1-day")'
 end
+
+every 1.day, :at => '10:56 am' do
+  runner 'FeedEntry.update_from_feed("http://feeds.feedburner.com/offtheback")'
+end
