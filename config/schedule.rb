26fb88d5685f06008bdc45ee129ed3bc6f447ac6
Bradley Priest <bradleypriest@gmail.com>
2010-05-18 12:02:12 +1200

    cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
-every 1.day, :at => '11:59 pm' do
+every 1.day, :at => '12:03 pm' do
   runner 'FeedEntry.update_from_feed("http://feed.1-day.co.nz/1-day")'
 end
 
-every 1.day, :at => '11:59 am' do
+every 1.day, :at => '12:03 am' do
   runner 'FeedEntry.update_from_feed("http://feeds.feedburner.com/offtheback")'
 end
