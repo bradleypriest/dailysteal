e5678ac648ac95a6f6fae16d399aad0062dfbb6c
Bradley Priest <bradleypriest@gmail.com>
2010-05-21 11:41:11 +1200

    two day history fixed ninja

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -22,6 +22,14 @@ class FeedsController < ApplicationController
       format.xml  { render :xml => @feed_entries }
     end
   end
+  def daybefore
+    @feeds = FeedEntry.all
+
+    respond_to do |format|
+      format.html # yesterday.html.erb
+      format.xml  { render :xml => @feed_entries }
+    end
+  end
   
   def faq
   end
