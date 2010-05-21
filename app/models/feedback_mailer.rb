32616f0e573693f0e540d0645fce8cff43a55c3d
Bradley Priest <bradleypriest@gmail.com>
2010-05-22 00:31:01 +1200

    got mail working

--- app/models/feedback_mailer.rb
+++ app/models/feedback_mailer.rb
@@ -2,7 +2,7 @@ class FeedbackMailer < ActionMailer::Base
   
   def feedback(feedback)
     @recipients  = 'bradleypriest@gmail.com'
-    @from        = 'bradleypriest@gmail.com'
+    @from        = 'feedback@dailysteal.com'
     @subject     = "[Feedback for Dailysteal] #{feedback.subject}"
     @sent_on     = Time.now
     @body[:feedback] = feedback    
