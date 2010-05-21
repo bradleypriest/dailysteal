16ad431237cae71e2528bdf8c9258862fa447a9f
Bradley Priest <bradleypriest@gmail.com>
2010-05-21 23:28:26 +1200

    removed mail

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,15 +1,3 @@
-class Feedback
-
-  attr_accessor :subject, :email, :comment    
-  
-  def initialize(params = {})
-    self.subject = params[:subject]
-    self.email = params[:email]
-    self.comment = params[:comment]
-  end
-  
-  def valid?
-    self.comment && !self.comment.strip.blank?
-  end
-
+class Feedback < ActiveRecord::Base
+  validates_presence_of :comment
 end
