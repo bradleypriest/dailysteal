60ca922bcf8f1a25eab88538d79ab2fd9ca34636
Bradley Priest <bradleypriest@gmail.com>
2010-05-21 21:53:59 +1200

    lo

--- feedback_hooks.rb
+++
@@ -1,7 +0,0 @@
-class FeedbackHooks < Spree::ThemeSupport::HookListener
-  insert_after :inside_head do
-    %(<!--<%= javascript_include_tag :defaults, :cache => "cache/all" %>-->
-    <%= feedback_includes %>
-    <%= feedback_tab(:position => 'left') %>)
-  end
-end
