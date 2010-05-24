6784d9ca58225cc59602ea1a8079d05d92d058d1
Bradley Priest <bradleypriest@gmail.com>
2010-05-25 10:56:48 +1200

    no idea

--- config/environment.rb
+++ config/environment.rb
@@ -15,10 +15,10 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "pauldix-feedzirra", :lib => "feedzirra", :source => "http://gems.github.com"
+ # config.gem "pauldix-feedzirra", :lib => "feedzirra", :source => "http://gems.github.com"
   config.gem 'javan-whenever', :lib => false, :source => 'http://gems.github.com'
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com' 
-  
+
   
  
 
