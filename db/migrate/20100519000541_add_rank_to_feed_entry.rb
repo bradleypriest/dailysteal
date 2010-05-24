2092636cc89c50d5470dd5079497a04de64c4099
Bradley Priest <bradleypriest@gmail.com>
2010-05-24 18:55:17 +1200

    fixed table

--- db/migrate/20100519000541_add_rank_to_feed_entry.rb
+++
@@ -1,9 +0,0 @@
-class AddRankToFeedEntry < ActiveRecord::Migration
-  def self.up
-    add_column :feed_entries, :role, :int
-  end
-
-  def self.down
-    remove_column :feed_entries, :role
-  end
-end
--- db/migrate/20100523034111_add_rank_to_product_entries.rb
+++
@@ -1,9 +0,0 @@
-class AddRankToProductEntries < ActiveRecord::Migration
-  def self.up
-    add_column :product_entries, :rank, :string
-  end
-
-  def self.down
-    remove_column :product_entries, :rank
-  end
-end
