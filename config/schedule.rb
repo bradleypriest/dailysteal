5412fb406481b18c6a736df62e055a51205c9918
Bradley Priest <bradleypriest@gmail.com>
2010-05-24 19:11:15 +1200

    as

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ every 1.day, :at => '12:02 am' do
   #Ziwi   
     rake "fetch_ziwi" 
 end
-every 1.day, :at => '7.11 am' do
+every 1.day, :at => '7.13 am' do
 #Daysale
   rake "fetch_daysale"
 #6Shooter
