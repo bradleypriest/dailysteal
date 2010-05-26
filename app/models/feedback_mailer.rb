89030be99e50a595d7d5a6063bf50223e1f5e6cc
Bradley Priest <bradleypriest@gmail.com>
2010-05-26 13:38:43 +1200

    added reply-to

--- app/models/feedback_mailer.rb
+++ app/models/feedback_mailer.rb
@@ -1,9 +1,10 @@
 class FeedbackMailer < ActionMailer::Base
   
   def feedback(feedback)
-    @recipients  = 'bradleypriest@gmail.com'
-    @from        = 'feedback@dailysteal.com'
+    @recipients  = 'admin@dailysteal.co.nz'
+    @from        = 'feedback@dailysteal.co.nz'
     @subject     = "[Feedback for Dailysteal] #{feedback.subject}"
+    @reply_to    = "#{feedback.email}"
     @sent_on     = Time.now
     @body[:feedback] = feedback    
   end
