f2edb191dda7b54c0eb7dacd6413d15150f48701
Bradley Priest <bradleypriest@gmail.com>
2010-06-01 13:14:44 +1200

    all

---
+++ db/migrate/20100529094033_create_signups.rb
@@ -0,0 +1,14 @@
+class CreateSignups < ActiveRecord::Migration
+    def self.up
+      create_table :signups do |t|
+        t.string :name
+        t.string :email
+
+        t.timestamps
+      end
+    end
+
+    def self.down
+      drop_table :signups
+    end
+  end
