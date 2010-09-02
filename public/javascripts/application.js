75a831859961cf8501009e3e3f7d532246f3c717
Bradley Priest <bradleypriest@gmail.com>
2010-09-02 18:55:37 +1200

    added autosizing

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+   var $winwidth = $(window).width();
+   var $width = Math.floor($winwidth/256);
+   var $fin = $width*256+'px';
+   $("#content").css({
+     width: $fin
+   });
+   $(window).bind("resize", function(){
+     var $winwidth = $(window).width();
+     var $width = Math.floor($winwidth/256);
+     var $fin = $width*256+'px';
+     $("#content").css({
+       width: $fin
+     });
+ });
+ });
