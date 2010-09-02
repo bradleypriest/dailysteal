4748bffb298441ba3ae1c1daa555a78a24e82cc8
Bradley Priest <bradleypriest@gmail.com>
2010-09-02 16:44:40 +1200

    rake

--- db/migrate/20100902000326_add_location_to_coupons.rb
+++
@@ -1,9 +0,0 @@
-class AddLocationToCoupons < ActiveRecord::Migration
-  def self.up
-    add_column :coupons, :location, :string
-  end
-
-  def self.down
-    remove_column :coupons, :location
-  end
-end
