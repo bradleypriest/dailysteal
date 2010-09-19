bbf9f156b3cf9c2a94a96a3bcca5d7ab4d716d3f
Bradley Priest <bradleypriest@gmail.com>
2010-09-20 11:56:50 +1200

    meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  # helper :all # include all helpers, all the time
-  #  attr_accessor :stylesheets
-  #  before_filter {|c| c.stylesheets ||= []; c.stylesheets << 'scaffold' }
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Agg::Application.routes.draw do
+  root :to => 'feeds#index'
+  
   match 'feedbacks' => 'feedbacks#create', :as => :feedback
   match 'feedbacks/new' => 'feedbacks#new', :as => :new_feedback
+  
   match 'coupons' => 'coupons#index', :as => :coupons
-  match 'coupons/:id.:format' => 'coupons#local', :as => :coupons
-  match '/' => 'feeds#index'
+  match 'coupons/:id.:format' => 'coupons#local', :as => :local_coupons
+  
   match '/about' => 'feeds#about', :as => :about
   match '/faq' => 'feeds#faq', :as => :faq
   match '/old/' => 'feeds#yesterday', :as => :yesterday
