f859cb96e249c35e2f7b965079bde0f59f2d0a0d
Bradley Priest <bradleypriest@gmail.com>
2011-10-30 11:03:44 +1300

    update?

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,5 @@
 Agg::Application.configure do
-  # Settings specified here will take precedence over those in config/environment.rb
+  # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
   # test suite.  You never need to work with it otherwise.  Remember that
@@ -32,6 +32,4 @@ Agg::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-  
-  
 end
