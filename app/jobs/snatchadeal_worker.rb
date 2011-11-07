7fbddc09781f58d759b915e5e57b1d2e7bb0ae10
Bradley Priest <bradleypriest@gmail.com>
2011-11-08 07:18:27 +1300

    ghetto snatchadeal

--- app/jobs/snatchadeal_worker.rb
+++ app/jobs/snatchadeal_worker.rb
@@ -10,7 +10,7 @@ class SnatchadealWorker < DJ::Worker
       published = PublishTime.new( 11 ).time
       guid = url[/\d+/]+(published.strftime(fmt='%d%m%g'))
       name = item.at_css("h2 a").text
-unless FeedEntry.where('published > ?', 2.days.ago).where('name like ?', "#{name[0..8]}%" ).exists?
+unless FeedEntry.where('published > ?', 2.weeks.ago).where('name like ?', "#{name[0..8]}%" ).exists?
       price = item.at_css(".home-daily-deal-pricing").text[/\$[\d,]+\.\d\d/]
       fullprice = item.at_css(".home-daily-deal-rrp").text[/\$[\d,]+\.\d\d/]
       picture = item.at_css("img")[:src].gsub(/\s/,'%20')
