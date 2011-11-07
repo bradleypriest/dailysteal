5c213f4b807119aae30e24823aff688c95c8679b
Bradley Priest <bradleypriest@gmail.com>
2011-11-07 19:11:22 +1300

    snatchadeal is too powerful

--- app/jobs/snatchadeal_worker.rb
+++ app/jobs/snatchadeal_worker.rb
@@ -6,11 +6,11 @@ class SnatchadealWorker < DJ::Worker
   doc = Nokogiri::HTML(open(url))
 
     doc.css(".home-daily-deal").each do |item|
-       url = item.at_css("h2 a")[:href]
-        published  = (Time.now+12.hours).hour>=11? Date.today+23.hours : Date.today-1.hours
-        guid = url[/\d+/]+(published.strftime(fmt='%d%m%g'))
-unless FeedEntry.exists? :guid => guid
+      url = item.at_css("h2 a")[:href]
+      published = PublishTime.new( 11 ).time
+      guid = url[/\d+/]+(published.strftime(fmt='%d%m%g'))
       name = item.at_css("h2 a").text
+unless FeedEntry.where('published > ?', 2.days.ago).where('name like ?', "#{name[0..8]}%" ).exists?
       price = item.at_css(".home-daily-deal-pricing").text[/\$[\d,]+\.\d\d/]
       fullprice = item.at_css(".home-daily-deal-rrp").text[/\$[\d,]+\.\d\d/]
       picture = item.at_css("img")[:src].gsub(/\s/,'%20')
