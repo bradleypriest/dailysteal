0748330bea1dd52ef87c3e15022cb0d25a9df40c
Bradley Priest <bradleypriest@gmail.com>
2011-11-09 22:49:54 +1300

    chronic is having a hissy fit

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,31 +2,30 @@ set :output, path+'/log/cron.log'
 
 set :job_template, nil
 
-every 1.day, :at => '12.02 am' do
+every 1.day, :at => '12.05am' do
   runner "GroupyWorker.enqueue"
   runner "GraboneWorker.enqueue"
   runner "SpreetsWorker.enqueue"
   runner "TreatmeWorker.enqueue"
 end
 
-every 1.day, :at => '9.02 am' do
+every 1.day, :at => '9.02am' do
   runner "DailydoWorker.enqueue"
 end
-every 1.day, :at => '10.02 am' do
+every 1.day, :at => '10.02am' do
   runner "DealadayWorker.enqueue"
   runner "TrademeWorker.enqueue"
   #runner "ZiwiWorker.enqueue"
 end
 
-every 1.day, :at => '11.01 am' do
+every 1.day, :at => '11.02am' do
   runner "SnatchadealWorker.enqueue"
 end
 
-every 1.day, :at => '12.01 pm' do
+every 1.day, :at => Time.now.at_beginning_of_day+12.hours+4.minutes do
   runner "OneDayWorker.enqueue"
   runner "SixShooterWorker.enqueue"
   runner "ThreeDealsWorker.enqueue"
-  #runner "MightyApeWorker.enqueue"
   runner "DaysaleWorker.enqueue"
   runner "CrazysalesWorker.enqueue"
 end
@@ -35,3 +34,4 @@ every '15,45 * * * *' do
  runner "StockWorker.enqueue"
 end
 
+#runner "MightyApeWorker.enqueue"
