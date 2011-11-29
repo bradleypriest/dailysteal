028fcf17ebf61f7334b71f835b40d122eaa5bd10
Bradley Priest <bradleypriest@gmail.com>
2011-11-30 07:45:13 +1300

    clean snatchadeal

--- app/jobs/snatchadeal_worker.rb
+++ app/jobs/snatchadeal_worker.rb
@@ -7,31 +7,33 @@ class SnatchadealWorker < DJ::Worker
 
     doc.css(".home-daily-deal").each do |item|
       url = item.at_css("h2 a")[:href]
-      published = PublishTime.new( 11 ).time
-      guid = url[/\d+/]+(published.strftime(fmt='%d%m%g'))
-      name = item.at_css("h2 a").text
-unless FeedEntry.where('published > ?', 2.weeks.ago).where('name like ?', "#{name[0..8]}%" ).exists?
-      price = item.at_css(".home-daily-deal-pricing").text[/\$[\d,]+\.\d\d/]
-      fullprice = item.at_css(".home-daily-deal-rrp").text[/\$[\d,]+\.\d\d/]
-      picture = item.at_css("img")[:src].gsub(/\s/,'%20')
-      stock = doc.at_css(".home-daily-deal-buy-now") ? 100 : 0
+      unless url[/d+/]
+        published = PublishTime.new( 11 ).time
+        guid = url[/\d+/]+(published.strftime(fmt='%d%m%g'))
+        name = item.at_css("h2 a").text
+        unless FeedEntry.where('published > ?', 2.weeks.ago).where('name like ?', "#{name[0..8]}%" ).exists?
+          price = item.at_css(".home-daily-deal-pricing").text[/\$[\d,]+\.\d\d/]
+          fullprice = item.at_css(".home-daily-deal-rrp").text[/\$[\d,]+\.\d\d/]
+          picture = item.at_css("img")[:src].gsub(/\s/,'%20')
+          stock = doc.at_css(".home-daily-deal-buy-now") ? 100 : 0
 
 
-    FeedEntry.create!(
-      :name       => name,
-      :price      => price,
-      :fullprice  => fullprice,
-      :url        => 'http://www.snatchadeal.co.nz/'+url,
-      :picture    => 'http://www.snatchadeal.co.nz/'+picture,
-      :published  => published,
-      :home       => 'Snatchadeal',
-      :home_url   => 'http://www.snatchadeal.co.nz/',
-      :guid       =>  guid,
-      :rank       =>  25,
-      :stock      => stock
-      )
+          FeedEntry.create!(
+            :name       => name,
+            :price      => price,
+            :fullprice  => fullprice,
+            :url        => 'http://www.snatchadeal.co.nz/'+url,
+            :picture    => 'http://www.snatchadeal.co.nz/'+picture,
+            :published  => published,
+            :home       => 'Snatchadeal',
+            :home_url   => 'http://www.snatchadeal.co.nz/',
+            :guid       =>  guid,
+            :rank       =>  25,
+            :stock      => stock
+          )
 
+        end
+      end
     end
   end
-end
 end
